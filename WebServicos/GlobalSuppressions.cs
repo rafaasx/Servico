d0dc95129c301e4d94b0fc72642be4f17b6d42a0
Rafael Xavier <rafaeltwisted@gmail.com>
2018-03-20 08:18:08 -0300

    Traduções

---
+++ WebServicos/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:WebServicos.Controllers.AccountController.SendCode(WebServicos.Models.SendCodeViewModel)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0037:Use inferred member name", Justification = "<Pending>", Scope = "member", Target = "~M:WebServicos.Controllers.AccountController.Login(WebServicos.Models.LoginViewModel,System.String)~System.Threading.Tasks.Task{System.Web.Mvc.ActionResult}")]
+
